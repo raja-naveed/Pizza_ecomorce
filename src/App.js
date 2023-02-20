19c391a635eef4f6f0273feb4453bcc8f08a6efe
“Raja <rajadahri11@gmail.com>
2023-02-20 14:04:51 +0500

    Added Tailwind Css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container flex flex-row items-center justify-center">
+      <h1 className="text text-xl">React App</h1>
+      <h2>This is tribute to Thalaivaaar</h2>
+    </div >
   );
 }
 
