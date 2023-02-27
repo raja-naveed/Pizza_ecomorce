b29e0c344d37f8919ad560f62fd21ccda0a0fe0a
“Raja <rajadahri11@gmail.com>
2023-02-28 00:34:48 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Products from "./pages/Products";
 import Navigation from "./components/Navigation";
-import Cart from "./pages/Cart";
-
 function App() {
   return (
     <Router>
             <Navigation />
             <Routes>
                 <Route path="/" element={<Home />} exact />
-                <Route path="/products" element={<Products />} />
-                <Route path="/cart" element={<Cart />} /> 
             </Routes>
     </Router>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-image': "url('/src/Assets/home-banner-background.png')"
+      }
+    },
   },
   plugins: [],
 }
